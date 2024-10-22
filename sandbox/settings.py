ad0e0b672a1b57ecc4096b5a152d4178523ccf18
arz <alirezazarini2019@gmail.com>
2024-10-22 20:27:00 +0100

    catalogue app

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -39,8 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    # "src/app-name"
+
+    # apps
+    'src.catalogue',
+
+    # third-party apps
+    'treebeard',
 ]
 
 MIDDLEWARE = [
