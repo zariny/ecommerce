9425156bce77a1d69ffda304413d36f67c9e4832
arz <alirezazarini2019@gmail.com>
2024-11-05 12:15:29 +0000

    custom lookup registered

--- src/catalogue/models.py
+++ src/catalogue/models.py
@@ -1,6 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
 from src.core.models import BaseSeoModel, ModelWithMetadata, TranslationModel
 from treebeard.mp_tree import MP_Node
+
+
+class ReverseStartsWithLookup(models.lookups.StartsWith):
+    """
+    just like normal (startswith) lookup but performs a reverse "starts with" query,
+     so that the left-hand side (lhs) and right-hand side (rhs) positions are swapped.
+
+    This lookup behaves similarly to Django's built-in `StartsWith` lookup,
+    but with the lhs and rhs reversed. It allows querying if a specified value
+    (rhs) "starts with" the field's value (lhs) instead of the typical order.
+
+    For example:
+        - my_field__rstartswith = "prefix"
+          will check if "prefix" starts with values in the field `my_field`.
+    """
+
+    def process_rhs(self, qn, connection):
+        return super().process_lhs(qn, connection)
+
+    def process_lhs(self, compiler, connection, lhs=None):
+        if lhs is not None:
+            raise Exception("Flipped process_lhs does not accept lhs argument")
+        return super().process_rhs(compiler, connection)
+
+
+models.Field.register_lookup(ReverseStartsWithLookup, "rstartswith")
 
 
 class Category(MP_Node, BaseSeoModel, ModelWithMetadata):
@@ -47,6 +74,30 @@ class Category(MP_Node, BaseSeoModel, ModelWithMetadata):
         slugs = [category.slug for category in self.get_ancestors()]
         slugs.append(self.slug)
         return self._full_slug_seperator.join(slugs)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def refresh_ancestors_are_public(self):
+        """
+            set correct value for (ancestors_are_public) field
+             via check ancestor's (is_public) field for each subtree.
+
+            - this method avoid run a new save for each updated object.
+        """
+        subquery = type(self)._default_manager.filter(
+            is_public=False,
+            path__rstartswith=models.OuterRef("path"),
+            depth__lt=models.OuterRef("depth")
+        )
+
+        self.get_tree(self).update(
+            ancestors_are_public=~models.Exists(subquery.values("pk"))
+        )
+
+        self.refresh_from_db()
 
     def has_children(self):
         return self.numchild > 0
