89e71940be8795bd9360c73a247f353d5ddbf9c3
arz <alirezazarini2019@gmail.com>
2025-02-21 12:42:20 +0330

    register serializer

--- src/account/serializers.py
+++ src/account/serializers.py
@@ -9,3 +9,24 @@ User = get_user_model()
 class UserAuthenticationSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     password = serializers.CharField(max_length=128, required=True, style={"input_type": "password"})
+
+
+class UserRegistrationSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True, validators=(UniqueValidator(queryset=User.objects.all()),))
+    password = serializers.CharField(max_length=128, required=True, style={"input_type": "password"})
+    password_again = serializers.CharField(max_length=128, required=True, style={"input_type": "password"})
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password_again"]:
+            message = {"password": "password do not match", "password_again": "password do not match"}
+            raise serializers.ValidationError(message)
+
+        try:
+            password_validation.validate_password(attrs["password"])
+        except ValidationError as e:
+            raise serializers.ValidationError({"password": str(e)})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data["email"], validated_data["password"])
+        return user
