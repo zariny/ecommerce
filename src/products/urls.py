5db0d92ef8d49cd7ec4e6c022834de5e5583c566
arz <alirezazarini2019@gmail.com>
2025-02-24 13:43:18 +0330

    dashboard urls

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .administration.urls import urlpatterns as admin_urlpatterns
 from . import views
 
 
 urlpatterns = [
     path("products/", views.ProductList.as_view()),
     path("product/<slug:slug>", views.ProductDetail.as_view()),
-]
+] + admin_urlpatterns
