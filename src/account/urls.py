2cc5fac9002a56cd346691beb337bf648125252b
arz <alirezazarini2019@gmail.com>
2025-02-26 18:05:23 +0330

    account dashboard endpoints

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .administration.urls import urlpatterns as admin_urlpatterns
 
 
 urlpatterns = [
     path("login/", views.UserAuthenticationView.as_view()),
-]
+    path("refresh/", views.TokenRefreshView.as_view()),
+    path("register/", views.UserRegistrationView.as_view()),
+] + admin_urlpatterns
