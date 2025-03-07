46db23b52979bf1faa6123221cb617e334d49ce0
arz <alirezazarini2019@gmail.com>
2025-03-08 01:11:39 +0330

    productclass endpoint api

--- src/products/administration/urls.py
+++ src/products/administration/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("admin/product/<int:pk>/", views.ProductRetrieveUpdateAdmin.as_view()),
     path("admin/attributes/", views.ProductAttributeListCreateAdmin.as_view()),
     path("admin/attribute/<int:pk>/", views.ProductAttributeDetailAdmin.as_view()),
+    path("admin/productclasses/", views.ProductClassListCreateAdmin.as_view()),
+    path("admin/productclass/<int:pk>/", views.ProductClassDetailAdmin.as_view()),
 ]
