47fa1e126e5d9a770bcbbdf05eba92f2b786949e
arz <alirezazarini2019@gmail.com>
2025-03-09 11:26:34 +0330

    dashboard views name has been change

--- src/products/administration/urls.py
+++ src/products/administration/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path("admin/products/", views.ProductListCreateAdmin.as_view()),
-    path("admin/product/<int:pk>/", views.ProductRetrieveUpdateAdmin.as_view()),
-    path("admin/attributes/", views.ProductAttributeListCreateAdmin.as_view()),
-    path("admin/attribute/<int:pk>/", views.ProductAttributeDetailAdmin.as_view()),
-    path("admin/productclasses/", views.ProductClassListCreateAdmin.as_view()),
-    path("admin/productclass/<int:pk>/", views.ProductClassDetailAdmin.as_view()),
+    path("admin/products/", views.ProductAdminView.as_view()),
+    path("admin/product/<int:pk>/", views.ProductDetailAdminView.as_view()),
+    path("admin/attributes/", views.ProductAttributeAdminView.as_view()),
+    path("admin/attribute/<int:pk>/", views.ProductAttributeDetailAdminView.as_view()),
+    path("admin/productclasses/", views.ProductClassAdminView.as_view()),
+    path("admin/productclass/<int:pk>/", views.ProductClassDetailAdminView.as_view()),
 ]
--- src/products/administration/views.py
+++ src/products/administration/views.py
@@ -1,9 +1,6 @@
-from rest_framework import generics
 from rest_framework.filters import SearchFilter
 from django_filters import rest_framework as filters
-from core.authenticate import JWTCookiesBaseAuthentication
-from core.permissions import AdminSafeOrModelLvlPermission
-from core.views import ListLimitOffsetPagination
+from core.views import BaseAdminView, BaseAdminDetailView
 from .. import models
 from . import serializers
 
@@ -19,7 +16,7 @@ class ProductFilterByCategoryAdmin(filters.FilterSet):
         fields = ("category",)
 
 
-class ProductListCreateAdmin(generics.ListCreateAPIView):
+class ProductAdminView(BaseAdminView):
     """
     API view for listing and creating products in the administration panel.
 
@@ -35,17 +32,14 @@ class ProductListCreateAdmin(generics.ListCreateAPIView):
         Pagination: The pagination class used for listing.
         search_fields: Fields that support search (title, slug).
     """
-    authentication_classes = (JWTCookiesBaseAuthentication,)
-    permission_classes = (AdminSafeOrModelLvlPermission,)
     queryset = models.Product.objects.only("pk", "title", "slug", "is_public", "updated_at")
     serializer_class = serializers.ProductAdminSerializer
     filter_backends = (filters.DjangoFilterBackend, SearchFilter)
     filterset_class = ProductFilterByCategoryAdmin
-    pagination_class = ListLimitOffsetPagination
     search_fields = ("title", "slug")
 
 
-class ProductRetrieveUpdateAdmin(generics.RetrieveUpdateAPIView):
+class ProductDetailAdminView(BaseAdminDetailView):
     """
     API view for retrieving and updating product details in the administration panel.
     Supports retrieving product details, including related product type and prefetching categories.
@@ -56,8 +50,6 @@ class ProductRetrieveUpdateAdmin(generics.RetrieveUpdateAPIView):
         data: The base queryset with selected fields and prefetching.
         lookup_field: The field used for lookup (slug).
     """
-    authentication_classes = (JWTCookiesBaseAuthentication,)
-    permission_classes = (AdminSafeOrModelLvlPermission,)
     queryset = models.Product.objects.select_related(
         "product_type"
     ).only(
@@ -67,37 +59,27 @@ class ProductRetrieveUpdateAdmin(generics.RetrieveUpdateAPIView):
     serializer_class = serializers.ProductDetailAdminSerializer
 
 
-class ProductAttributeListCreateAdmin(generics.ListCreateAPIView):
-    authentication_classes = (JWTCookiesBaseAuthentication,)
-    permission_classes = (AdminSafeOrModelLvlPermission,)
+class ProductAttributeAdminView(BaseAdminView):
     queryset = models.ProductAttribute.objects.only("pk", "name", "slug", "value_type")
     serializer_class = serializers.ProductAttributeAdminSerializer
     filter_backends = (filters.DjangoFilterBackend, SearchFilter)
-    pagination_class = ListLimitOffsetPagination
     filterset_fields = ("require", "value_type")
     search_fields = ("slug", "name")
 
 
-class ProductAttributeDetailAdmin(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = (JWTCookiesBaseAuthentication,)
-    permission_classes = (AdminSafeOrModelLvlPermission,)
+class ProductAttributeDetailAdminView(BaseAdminDetailView):
     queryset = models.ProductAttribute.objects.all()
     serializer_class = serializers.ProductAttributeDetailAdminSerializer
 
 
-class ProductClassListCreateAdmin(generics.ListCreateAPIView):
-    authentication_classes = (JWTCookiesBaseAuthentication,)
-    permission_classes = (AdminSafeOrModelLvlPermission,)
+class ProductClassAdminView(BaseAdminView):
     queryset = models.ProductClass.objects.all()
     serializer_class = serializers.ProductClassAdminSerializer
     filter_backends = (filters.DjangoFilterBackend, SearchFilter)
-    pagination_class = ListLimitOffsetPagination
     filterset_fields = ("require_shipping", "track_stock", "abstract")
     search_fields = ("slug", "title")
 
 
-class ProductClassDetailAdmin(generics.RetrieveUpdateDestroyAPIView):
-    authentication_classes = (JWTCookiesBaseAuthentication,)
-    permission_classes = (AdminSafeOrModelLvlPermission,)
+class ProductClassDetailAdminView(BaseAdminDetailView):
     queryset = models.ProductClass.objects.all()
     serializer_class = serializers.ProductClassDetailAdminSerializer
