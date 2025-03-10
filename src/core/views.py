a9fe67e8b97634ffb564e854d08c9628eba23ce9
arz <alirezazarini2019@gmail.com>
2025-03-10 12:17:58 +0330

    Base admin views in core app mplemented

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,19 @@
+from rest_framework import generics
 from rest_framework.pagination import LimitOffsetPagination
+from . import permissions, authenticate
 
 
 class ListLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 10
     max_limit = 30
+
+
+class BaseAdminView(generics.ListCreateAPIView):
+    authentication_classes = (authenticate.JWTCookiesBaseAuthentication,)
+    permission_classes = (permissions.AdminSafeOrModelLvlPermission,)
+    pagination_class = ListLimitOffsetPagination
+
+
+class BaseAdminDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = (authenticate.JWTCookiesBaseAuthentication,)
+    permission_classes = (permissions.AdminSafeOrModelLvlPermission,)
