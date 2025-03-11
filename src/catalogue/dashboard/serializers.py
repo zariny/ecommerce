e9c01d989c1e5579576727b9c80d9209a1ee7729
arz <alirezazarini2019@gmail.com>
2025-03-11 18:02:20 +0330

    Implementing the move mechanism for tree serializers

--- src/catalogue/dashboard/serializers.py
+++ src/catalogue/dashboard/serializers.py
@@ -14,11 +14,34 @@ class BaseCategoryAdminSerializer(serializers.ModelSerializer):
         label="Relative to"
     )
 
+    def move(self, instance, position, relative_to=None):
+        if position == "root":
+            if instance.is_root():
+                return instance
+            root_node = models.Category.get_root_nodes().first()
+            if root_node is None:
+                raise ValidationError({"position": ["Cannot move to root because no root nodes exist."]})
+            instance.move(root_node, pos="last-sibling")
+        else:
+            if relative_to is None:
+                raise ValidationError({"relative_to": ["This field is required when moving the category."]})
+            elif position == "after":
+                instance.move(relative_to, pos="right")
+            elif position == "before":
+                instance.move(relative_to, pos="left")
+            elif position == "first_child_of":
+                instance.move(target=relative_to, pos="first-child")
+
+        instance.refresh_from_db()
+        return instance
+
     def update(self, instance, validated_data):
         position = validated_data.pop("position", None)
+        relative_to = validated_data.pop("relative_to", None)
+        instance = super().update(instance, validated_data)
+        if position is not None:
+            self.move(instance, position, relative_to)
         return instance
-        ...
-
 
     def create(self, validated_data):
         position = validated_data.pop("position")
@@ -41,9 +64,6 @@ class BaseCategoryAdminSerializer(serializers.ModelSerializer):
 class CategoryAdminSerializer(BaseCategoryAdminSerializer):
     class Meta:
         model = models.Category
-        # fields = ("pk", "name", "is_public", "ancestors_are_public", "is_root", "depth", "path") #unable to resolve type hint for
-        # function "is_root". Consider using a type hint or @extend_schema_field. Defaulting to string.
-
         fields = (
             "pk", "name", "slug", "is_public", "ancestors_are_public", "is_root", "description", "meta_title",
             "meta_description", "metadata", "updated_at", "background", "background_caption", "position", "relative_to",
@@ -79,12 +99,26 @@ class CategoryDeatilAdminSerializer(BaseCategoryAdminSerializer):
 
     class Meta:
         model = models.Category
-        # exclude = ("path", "depth", "numchild")
         fields = (
             "pk", "name", "slug", "full_name", "is_public", "ancestors_are_public", "description", "meta_title",
             "meta_description", "metadata", "updated_at", "background", "background_caption", "position", "relative_to",
         )
-        # extra_kwargs = {
-        #     "depth": {"write_only": True},
-        #     "path": {"write_only": True}
-        # }
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.is_root():
+            position = "root"
+            relative_to = dict()
+        else:
+            relative_node = instance.get_prev_sibling()
+            if relative_node is None:  # it was the leftmost sibling.
+                position = "first_child_of"
+                relative_to = instance.get_parent()
+                relative_to = {"pk": relative_to.pk, "name": relative_to.name, "is_public": relative_to.is_public}
+            else:
+                position = "after"
+                relative_to = {"pk": relative_node.pk, "name": relative_node.name, "is_public": relative_node.is_public}
+
+        representation["position"] = position
+        representation["relative_to"] = relative_to
+        return representation
