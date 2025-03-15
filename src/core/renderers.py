c7e9195ac4d95417800765483ed0353e1db6b3f4
TaymaZ <taymazak1382@gmail.com>
2025-03-15 20:15:38 +0330

    fix: improve structured JSON response handling and ensure data integrity

--- src/core/renderers.py
+++ src/core/renderers.py
@@ -4,16 +4,24 @@ from rest_framework.renderers import JSONRenderer
 class StructuredJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         if renderer_context is not None:
-            has_exception = renderer_context["response"].exception
-            status_code = renderer_context["response"].status_code
+            response = renderer_context["response"]
+            has_exception = response.exception
+            status_code = response.status_code
             success_flag: bool = not has_exception and status_code < 400
-            default_massaage = "Succesfully" if success_flag else "Failed"
-            message: str = data.pop("detail", default_massaage)
-            data = {
+            default_message = "Successfully" if success_flag else "Failed"
+
+            # Ensure `data` is a dictionary before using `.pop()`
+            data = data or {}
+
+            message: str = data.pop("detail", default_message)
+            structured_data = {
                 "success": success_flag,
                 "status": status_code,
                 "message": message,
                 "data": data if success_flag else None,
                 "errors": data if not success_flag else None
             }
+
+            return super().render(structured_data, accepted_media_type, renderer_context)
+
         return super().render(data, accepted_media_type, renderer_context)
