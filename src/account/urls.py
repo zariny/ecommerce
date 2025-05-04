8d0f6ee5607884e744e774e1b6d298058e1cd700
arz <alirezazarini2019@gmail.com>
2025-05-04 19:10:09 +0330

    include account/dashboard urls

--- src/account/urls.py
+++ src/account/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-from .administration.urls import urlpatterns as admin_urlpatterns
 
 
 urlpatterns = [
     path("login/", views.UserAuthenticationView.as_view()),
     path("refresh/", views.TokenRefreshView.as_view()),
     path("register/", views.UserRegistrationView.as_view()),
-] + admin_urlpatterns
+    path("admin/", include("account.dashboard.urls"))
+]
