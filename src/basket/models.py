64b62f47a5d7cd4a6825b7f172af7bf202fb9a96
arz <alirezazarini2019@gmail.com>
2025-05-07 19:25:30 +0330

    include pipeline for Basket model

--- src/basket/models.py
+++ src/basket/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from core.models import DatedModel
 from core.utils import currencies
+from uuid import uuid4
 
 
 class Basket(DatedModel):
     owner = models.ForeignKey("account.User", on_delete=models.CASCADE, related_name="carts", null=True)
+    token = models.UUIDField(default=uuid4, editable=False)
+    OPEN, MERGED, SAVED, FROZEN, SUBMITTED = (
+        "Open",
+        "Merged",
+        "Saved",
+        "Frozen",
+        "Submitted",
+    )
+    STATUS_CHOICES = (
+        (OPEN, "Open - currently active"),
+        (MERGED, "Merged - superceded by another basket"),
+        (SAVED, "Saved - for items to be purchased later"),
+        (FROZEN, "Frozen - the basket cannot be modified"),
+        (SUBMITTED, "Submitted - has been ordered at the checkout"),
+    )
+    status = models.CharField(max_length=128, default=OPEN, choices=STATUS_CHOICES)
 
 
 class Line(DatedModel):
@@ -13,7 +30,12 @@ class Line(DatedModel):
     stockrecord = models.ForeignKey("inventory.StockRecord", on_delete=models.CASCADE, related_name="basket_lines")
     quantity = models.PositiveIntegerField(default=1)
     price_currency = models.CharField(max_length=4 ,choices=currencies, default=currencies[0][0])
-    price_excl_tax = models.DecimalField("Price excluding tax", decimal_places=2, max_digits=12, null=True)
+    price_excl_tax = models.DecimalField(
+        "Price excluding tax",
+        decimal_places=2,
+        max_digits=12,
+        null=True
+    )
     price_incl_tax = models.DecimalField("Price includes tax", decimal_places=2, max_digits=12, null=True)
     tax_code = models.CharField(verbose_name="VAT rate code", max_length=64, blank=True, null=True)
 
