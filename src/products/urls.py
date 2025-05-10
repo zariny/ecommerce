2dd18d947f8d548e2a9431ca9808213261cd8fe1
arz <alirezazarini2019@gmail.com>
2025-05-10 15:52:28 +0330

    include product dashboard url patterns

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from .administration.urls import urlpatterns as admin_urlpatterns
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
     path("products/", views.ProductList.as_view()),
     path("product/<slug:slug>", views.ProductDetail.as_view()),
-] + admin_urlpatterns
+    path("admin/", include("products.dashboard.urls")),
+]
