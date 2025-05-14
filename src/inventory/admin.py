9ea3def61a8cf51ce487f403d93aebdb81bf1cf4
arz <alirezazarini2019@gmail.com>
2025-05-14 17:09:49 +0330

    Stockrecord registered in admin panel

--- src/inventory/admin.py
+++ src/inventory/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from core.admin import AbstractPieChartModelAdmin
 from . import models
 
 
-
-admin.site.register(models.StockRecord)
+@admin.register(models.StockRecord)
+class StockRecordAdmin(AbstractPieChartModelAdmin):
+    autocomplete_fields = ("product", )
+    search_fields = ("product__title", "pk")
