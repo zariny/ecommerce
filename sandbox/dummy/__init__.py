5feac7dd4a1bfa4d9394e2eea278dde7b58c524d
arz <alirezazarini2019@gmail.com>
2025-05-28 13:07:34 +0330

    renamed:sandbox/dummy_data/ -> sandbox/dummy/

--- sandbox/dummy/__init__.py
+++ sandbox/dummy/__init__.py
@@ -1,11 +1,15 @@
+from django.contrib.auth import get_user_model
 from .dummy_users import DummyUsersHandler
 from .dummy_categories import DummyCategoryHandler
 from .dummy_orders import DummyOrderHandler
 from .dummy_products import DummyProductHandler
 
 
+User = get_user_model()
+
+
 def run(flag): 	# Chain of Responsibility
-    if flag:
+    if flag and not User.objects.exists():
         users = DummyUsersHandler()
         products = DummyProductHandler()
         products.set_next(DummyOrderHandler())
