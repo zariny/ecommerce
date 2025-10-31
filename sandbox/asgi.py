fc23d7929612ede76b585aa786ca9a84eda98884
arz <alirezazarini2019@gmail.com>
2025-11-01 00:34:48 +0330

    Fixed -> correct import path for middleware.authentication

--- sandbox/asgi.py
+++ sandbox/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from middleware.jwt import JWTCookieAuthMiddleware
+from middleware.authentication import JWTCookieAuthMiddleware
 from account.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sandbox.settings')
