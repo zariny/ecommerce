9f92508ea57ab698ba1c1075a20458c1aed5cf93
arz <alirezazarini2019@gmail.com>
2026-07-19 16:04:04 +0330

    removed websocket support temporarily

--- sandbox/asgi.py
+++ sandbox/asgi.py
@@ -2,22 +2,19 @@
 ASGI config for ecommerce project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from middleware.authentication import JWTCookieAuthMiddleware
-from account.routing import websocket_urlpatterns
+# from middleware.authentication import JWTCookieAuthMiddleware
+# from account.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sandbox.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": JWTCookieAuthMiddleware(
-        URLRouter(websocket_urlpatterns)
-    ),
+    # "websocket": JWTCookieAuthMiddleware(
+    #     URLRouter(websocket_urlpatterns)
+    # ),
 })
--- sandbox/schema.py
+++
@@ -1,9 +0,0 @@
-import strawberry
-
-
-@strawberry.type
-class Query:
-    test: str = strawberry.field(lambda: "Hello, world!")
-
-
-schema = strawberry.Schema(query=Query)
