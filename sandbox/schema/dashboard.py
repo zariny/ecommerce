b9682744bfcd9dd1a886517b88ba5e221585934c
arz <alirezazarini2019@gmail.com>
2026-07-20 23:46:16 +0330

    added DjangoOptimizerExtension tool to prevent n+1 query problem

--- sandbox/schema/dashboard.py
+++ sandbox/schema/dashboard.py
@@ -1,10 +1,17 @@
 import strawberry
-from catalogue.dashboard import CatalogueQuery, CatalogueMutation
+from strawberry.schema.config import StrawberryConfig
+from catalogue.dashboard import CatalogueQuery
+from strawberry_django.optimizer import DjangoOptimizerExtension
 
 
 @strawberry.type
 class Query(CatalogueQuery):
-    test: str = strawberry.field(lambda: "Hello world")
+    node: strawberry.relay.Node = strawberry.relay.node()
+    nodes: list[strawberry.relay.Node] = strawberry.relay.node()
 
 
-schema = strawberry.Schema(query=Query)
+schema = strawberry.Schema(
+    query=Query,
+    config=StrawberryConfig(relay_max_results=10),
+    extensions=[DjangoOptimizerExtension],
+)
