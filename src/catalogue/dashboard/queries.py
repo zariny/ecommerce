055cd828b8d75e3868154ff6cf0aa37a4675381b
arz <alirezazarini2019@gmail.com>
2026-07-21 16:58:25 +0330

    using utils.relay.CursorConnection

--- src/catalogue/dashboard/queries.py
+++ src/catalogue/dashboard/queries.py
@@ -1,9 +1,9 @@
 import strawberry
 from strawberry_django import connection
-from strawberry_django.relay import DjangoCursorConnection
+from utils.relay import CursorConnection
 from .types import CategoryType
 
 
 @strawberry.type
 class CatalogueQuery:
-    categories: DjangoCursorConnection[CategoryType] = connection(max_results=10)
+    categories: CursorConnection[CategoryType] = connection(max_results=20)
