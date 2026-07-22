487a7d8abad9c1bc9ed8892c0d0a751fe1fa0776
arz <alirezazarini2019@gmail.com>
2026-07-23 00:17:58 +0330

    REBASE : rest api no longer support

--- src/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path("products/", views.ProductList.as_view()),
-    path("product/<slug:slug>", views.ProductDetail.as_view()),
-    path("admin/", include("products.dashboard.urls")),
-]
