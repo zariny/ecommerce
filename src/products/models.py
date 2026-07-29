6e157dea8f077b1d8a777d0731978571afb500be
arz <alirezazarini2019@gmail.com>
2026-07-29 13:08:41 +0330

    code formatting via ruff

--- src/products/models.py
+++ src/products/models.py
@@ -1,29 +1,46 @@
-from django.db import models
 from django.core.exceptions import ValidationError
-from utils.models import ModelWithDescription, BaseSeoModel, ModelWithMetadata, TranslationModel, SortableModel
-from .utils import VALUE_TYPE_CHOICE
-from .fields import DynamicValueField
-from .validation import validate_no_cycles
-from .exceptions import CycleInheritanceError
-from .managers import ProductQuerySet
+from django.db import models
+
+from utils.models import (
+    BaseSeoModel,
+    ModelWithDescription,
+    ModelWithMetadata,
+    SortableModel,
+    TranslationModel,
+)
+
 from .attr_container import ProductAttributeContainer
+from .exceptions import CycleInheritanceError
+from .fields import DynamicValueField
+from .managers import ProductQuerySet
+from .utils import VALUE_TYPE_CHOICE
+from .validation import validate_no_cycles
 
 
 class ProductClass(ModelWithMetadata):
     """
-        Each instance of ProductClass can have multiple inheritances from other instances and
-         inherit attributes and options from its ancestor classes.
+    Each instance of ProductClass can have multiple inheritances from other instances and
+     inherit attributes and options from its ancestor classes.
     """
+
     title = models.CharField(max_length=250, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, allow_unicode=True, auto_created="title", db_index=True)
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        allow_unicode=True,
+        auto_created="title",
+        db_index=True,
+    )
     require_shipping = models.BooleanField(default=True)
     track_stock = models.BooleanField(default=True)
-    abstract = models.BooleanField(default=False) # If True, this product class cannot have any product
+    abstract = models.BooleanField(
+        default=False
+    )  # If True, this product class cannot have any product
     bases = models.ManyToManyField(
         "self",
         symmetrical=False,
         through="products.ProductClassRelation",
-        related_name="subclasses"
+        related_name="subclasses",
     )
 
     class Meta:
@@ -34,7 +51,7 @@ class ProductClass(ModelWithMetadata):
         return self.title or self.slug
 
     def __repr__(self):
-        return "<%s> obj %s" % (type(self).__name__, self.title or self.slug)
+        return f"<{type(self).__name__}> obj {self.title or self.slug}"
 
     def get_ancestors(self, and_self=False) -> set:
         ancestors = set()
@@ -62,34 +79,48 @@ class ProductClass(ModelWithMetadata):
 
     def get_attributes(self, **filters):
         attributes = self.attributes.model.objects.filter(
-            product_class__in=self.get_ancestors(and_self=True),
-            **filters
+            product_class__in=self.get_ancestors(and_self=True), **filters
         ).distinct()
         return attributes
 
 
 class ProductClassRelation(models.Model):
-    subclass = models.ForeignKey("products.ProductClass", on_delete=models.CASCADE, related_name="base_relations")
-    base = models.ForeignKey("products.ProductClass", on_delete=models.CASCADE, related_name="sub_relations")
+    subclass = models.ForeignKey(
+        "products.ProductClass", on_delete=models.CASCADE, related_name="base_relations"
+    )
+    base = models.ForeignKey(
+        "products.ProductClass", on_delete=models.CASCADE, related_name="sub_relations"
+    )
 
     class Meta:
         unique_together = (("base", "subclass"),)
         app_label = "products"
 
     def __str__(self):
-        return "%s inherits from %s" % (self.subclass, self.base)
+        return f"{self.subclass} inherits from {self.base}"
 
     def cycle_relation_validation(self) -> None:
         if self.base == self.subclass:
             raise ValidationError("Self-inheritance is not allowed.")
 
         # Any two instances of the ProductClass should have only one relationship with each other
-        if self.base.pk and self.subclass.pk:
-            if type(self)._default_manager.filter(base=self.subclass, subclass=self.base).exists():
-                raise ValidationError("The inverse of this relationship has already been stored.")
+        if (
+            self.base.pk
+            and self.subclass.pk
+            and (
+                type(self)
+                ._default_manager.filter(base=self.subclass, subclass=self.base)
+                .exists()
+            )
+        ):
+            raise ValidationError(
+                "The inverse of this relationship has already been stored."
+            )
 
         try:
-            validate_no_cycles(base_product_kls=self.base, sub_product_kls=self.subclass)
+            validate_no_cycles(
+                base_product_kls=self.base, sub_product_kls=self.subclass
+            )
         except CycleInheritanceError as e:
             raise ValidationError(message=e.message)
 
@@ -99,15 +130,25 @@ class ProductClassRelation(models.Model):
 
 
 class Product(BaseSeoModel, ModelWithDescription):
-    product_type = models.ForeignKey("products.ProductClass", on_delete=models.PROTECT, related_name="products")
+    product_type = models.ForeignKey(
+        "products.ProductClass", on_delete=models.PROTECT, related_name="products"
+    )
     title = models.CharField(max_length=250, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True, allow_unicode=True, auto_created="title", db_index=True)
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        allow_unicode=True,
+        auto_created="title",
+        db_index=True,
+    )
     is_public = models.BooleanField(default=True)
-    attributes = models.ManyToManyField("products.ProductAttribute", through="products.ProductAttributeValue")
+    attributes = models.ManyToManyField(
+        "products.ProductAttribute", through="products.ProductAttributeValue"
+    )
     categories = models.ManyToManyField(
         "catalogue.Category",
         through="catalogue.ProductCategory",
-        related_name="products"
+        related_name="products",
     )
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
@@ -126,13 +167,15 @@ class Product(BaseSeoModel, ModelWithDescription):
         return self.title or self.slug
 
     def __repr__(self):
-        return "<%s> obj %s" % (type(self).__name__, self.title or self.slug)
+        return f"<{type(self).__name__}> obj {self.title or self.slug}"
 
     def clean(self):
         super().clean()
         if self.product_type_id and self.product_type.abstract:
             raise ValidationError(
-                {"product_type": "Abstract product type %s can not have any product." % self.product_type}
+                {
+                    "product_type": f"Abstract product type {self.product_type} can not have any product."
+                }
             )
 
     def refresh_from_db(self, using=None, fields=None, from_queryset=None):
@@ -142,10 +185,14 @@ class Product(BaseSeoModel, ModelWithDescription):
 
 
 class ProductAttribute(models.Model):
-    product_class = models.ManyToManyField("products.ProductClass", related_name="attributes", blank=True)
+    product_class = models.ManyToManyField(
+        "products.ProductClass", related_name="attributes", blank=True
+    )
     name = models.CharField(max_length=250)
     slug = models.SlugField(max_length=128, unique=True)
-    value_type = models.CharField(max_length=20, choices=VALUE_TYPE_CHOICE, default=VALUE_TYPE_CHOICE[0][0])
+    value_type = models.CharField(
+        max_length=20, choices=VALUE_TYPE_CHOICE, default=VALUE_TYPE_CHOICE[0][0]
+    )
     require = models.BooleanField(default=False)
 
     class Meta:
@@ -156,7 +203,9 @@ class ProductAttribute(models.Model):
 
 
 class ProductAttributeValue(models.Model):
-    product = models.ForeignKey("products.Product", on_delete=models.CASCADE, related_name="attribute_values")
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.CASCADE, related_name="attribute_values"
+    )
     attribute = models.ForeignKey("products.ProductAttribute", on_delete=models.CASCADE)
     value = DynamicValueField()
 
@@ -169,14 +218,15 @@ class ProductAttributeValue(models.Model):
 
     _default_representation = "<No Display>"
 
-
     @property
     def data_type(self):
         return self.attribute.value_type
 
 
 class ProductMedia(ModelWithMetadata, SortableModel):
-    product = models.ForeignKey("products.Product", on_delete=models.CASCADE, related_name="medias")
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.CASCADE, related_name="medias"
+    )
     image = models.ImageField(upload_to="products", blank=True, null=True)
     caption = models.CharField(max_length=250, blank=True)
     published = models.BooleanField(default=False)
@@ -186,53 +236,53 @@ class ProductMedia(ModelWithMetadata, SortableModel):
 
 
 class ProductTranslate(TranslationModel):
-    product = models.ForeignKey("products.Product", on_delete=models.CASCADE, related_name="translations")
+    product = models.ForeignKey(
+        "products.Product", on_delete=models.CASCADE, related_name="translations"
+    )
     title = models.CharField(max_length=250)
     description = models.TextField(blank=True)
 
     class Meta:
-        indexes = (
-            models.Index(fields=["product"]),
-        )
+        indexes = (models.Index(fields=["product"]),)
         app_label = "products"
         unique_together = (("language_code", "product"),)
         verbose_name_plural = "translation of products"
 
     def __str__(self):
-        return "%s - %s" % (self.product.title, self.title or self._default_presentation)
+        return f"{self.product.title} - {self.title or self._default_presentation}"
 
 
 class ProductAttributeTranslate(TranslationModel):
-    attribute = models.ForeignKey("products.ProductAttribute", on_delete=models.CASCADE, related_name="translations")
+    attribute = models.ForeignKey(
+        "products.ProductAttribute",
+        on_delete=models.CASCADE,
+        related_name="translations",
+    )
     name = models.CharField(max_length=250)
 
     class Meta:
-        indexes = (
-            models.Index(fields=["attribute"]),
-        )
+        indexes = (models.Index(fields=["attribute"]),)
         app_label = "products"
         unique_together = (("language_code", "attribute"),)
         verbose_name_plural = "translation of product attributes"
 
     def __str__(self):
-        return "%s - %s" % (self.attribute.name, self.name or self._default_presentation)
+        return f"{self.attribute.name} - {self.name or self._default_presentation}"
 
 
 class ProductAttributeValueTranslate(TranslationModel):
     attribute_value = models.ForeignKey(
         "products.ProductAttributeValue",
         on_delete=models.CASCADE,
-        related_name="translations"
+        related_name="translations",
     )
     value = models.TextField(blank=True)
 
     class Meta:
-        indexes = (
-            models.Index(fields=["attribute_value"]),
-        )
+        indexes = (models.Index(fields=["attribute_value"]),)
         app_label = "products"
         unique_together = (("language_code", "attribute_value"),)
         verbose_name_plural = "translation of product attribute value"
 
     def __str__(self):
-        return "%s - %s" % (self.attribute_value.attribute.name, self.value or self._default_presentation)
+        return f"{self.attribute_value.attribute.name} - {self.value or self._default_presentation}"
