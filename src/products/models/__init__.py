216f58400bee240a1a6e04ff24579749b6530e8b
arz <alirezazarini2019@gmail.com>
2026-08-16 13:59:19 +0330

    add model.variant models to __init file

--- src/products/models/__init__.py
+++ src/products/models/__init__.py
@@ -2,15 +2,27 @@ from .attributes import Attribute, AttributeTranslation
 from .classes import ProductClass, ProductClassEdge
 from .products import Product, ProductMedia, ProductTranslation
 from .values import AttributeValue, AttributeValueTranslation
+from .variants import (
+    AssignedVariantAttribute,
+    AssignedVariantAttributeValue,
+    AttributeVariant,
+    ProductVariant,
+    ProductVariantTranslation,
+)
 
 __all__ = [
+    "AssignedVariantAttribute",
+    "AssignedVariantAttributeValue",
     "Attribute",
     "AttributeTranslation",
     "AttributeValue",
     "AttributeValueTranslation",
+    "AttributeVariant",
     "Product",
     "ProductClass",
     "ProductClassEdge",
     "ProductMedia",
-    "ProductTranslation"
+    "ProductTranslation",
+    "ProductVariant",
+    "ProductVariantTranslation",
 ]
