c95d3f1b031764a8795e39cbf2a2e3745cf65251
arz <alirezazarini2019@gmail.com>
2026-09-03 13:56:38 +0330

    add Mutation root type into dashboard GraphQL schema

--- sandbox/schema/dashboard.py
+++ sandbox/schema/dashboard.py
@@ -1,17 +1,25 @@
 import strawberry
 from strawberry.schema.config import StrawberryConfig
-from catalogue.dashboard import CatalogueQuery
 from strawberry_django.optimizer import DjangoOptimizerExtension
+from catalogue.dashboard import CatalogueQuery
+from products.dashboard import ProductsQuery
+from authentication import AuthenticateMutation
 
 
 @strawberry.type
-class Query(CatalogueQuery):
+class Query(CatalogueQuery, ProductsQuery):
     node: strawberry.relay.Node = strawberry.relay.node()
     nodes: list[strawberry.relay.Node] = strawberry.relay.node()
 
 
+@strawberry.type
+class Mutation(AuthenticateMutation):
+    pass
+
+
 schema = strawberry.Schema(
     query=Query,
-    config=StrawberryConfig(relay_max_results=10),
+    mutation=Mutation,
+    config=StrawberryConfig(relay_max_results=25),
     extensions=[DjangoOptimizerExtension],
 )
